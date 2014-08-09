f8d6ebaf7007dc4773b3dc5a2aec8e72c84f947f
bhrw <bwo@umcg.nl>
2014-08-09 23:17:27 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,14 @@
+#load the data, subset the specific dates, convert date format
 source("Load_the_data.R")
 
+#output to png file
 png("plot1.png", width=480, height=480)
 
+#create the histogram
 hist(power2$Global_active_power, col="red", 
 main="Global Active Power",
 xlab="Global Active Power (kilowatts)", 
 ylab="Frequency")
 
+#back to console
 dev.off()
