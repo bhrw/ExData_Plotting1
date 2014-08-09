d593ef30e35adc01d14307993df541594ace46d9
bhrw <bwo@umcg.nl>
2014-08-09 22:59:53 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,8 @@
+png("plot2.png", width=480, height=480) 
+
+plot(power2$Time, power2$Global_active_power, 
+      type="l", 
+      xlab="", 
+      ylab="Global Active Power (kilowatts)") 
+dev.off() 
+Enter file contents here
